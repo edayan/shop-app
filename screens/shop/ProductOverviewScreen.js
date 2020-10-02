447221c891c3d9f0ed2c8de31e4cb700af7192c3
edayan <edayan.exgratia@gmail.com>
2020-10-02 22:39:07 +0530

    styling product item

--- screens/shop/ProductOverviewScreen.js
+++ screens/shop/ProductOverviewScreen.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { StyleSheet, Text, View, FlatList } from 'react-native'
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { FlatList, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
+import ProductItem from '../../components/shop/ProductItem';
 
 
 const ProductOverviewScreen = (props) => {
@@ -8,12 +9,18 @@ const ProductOverviewScreen = (props) => {
     return (
         <FlatList data={products}
             keyExtractor={item => item.id}
-            renderItem={itemData => <Text>{itemData.item.title}</Text>} />
+            renderItem={itemData => <ProductItem
+                image={itemData.item.imageUrl}
+                title={itemData.item.title}
+                price={itemData.item.price}
+                onViewDetail={() => { }}
+                onAddToCart={() => { }}
+            />} />
     )
 }
 
 ProductOverviewScreen.navigationOptions = {
-    headerTitle : 'All products'
+    headerTitle: 'All products'
 }
 
 export default ProductOverviewScreen
