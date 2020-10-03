1928ddcb75352bab021258e4c86a2940b2576db2
edayan <edayan.exgratia@gmail.com>
2020-10-03 10:08:23 +0530

    styling product details screen

--- screens/shop/ProductDetailScreen.js
+++ screens/shop/ProductDetailScreen.js
@@ -1,16 +1,22 @@
-import React from 'react'
-import { StyleSheet, Text, View, Button, ScrollView } from 'react-native'
+import React from 'react';
+import { Button, Image, ScrollView, StyleSheet, Text, View } from 'react-native';
 import { useSelector } from 'react-redux';
-
+import Colors from '../../constants/Colors';
 const ProductDetailScreen = props => {
     const productId = props.navigation.getParam('productId');
     const selectedProduct = useSelector(state => state.products.availableProducts.find(prod => prod.id === productId));
     return (
-        <View>
-            <Text>Product detail screens</Text>
-            <Text>{selectedProduct.title}</Text>
+        <ScrollView>
+            <Image style={styles.image} source={{ uri: selectedProduct.imageUrl }} />
+            <View style={styles.actions}>
+                <Button color={Colors.primary} title="Add to cart" onPress={() => { }} />
+            </View>
 
-        </View>
+
+            <Text style={styles.price}>${selectedProduct.price.toFixed(2)}</Text>
+            <Text style={styles.description}>{selectedProduct.description}</Text>
+
+        </ScrollView>
     )
 }
 ProductDetailScreen.navigationOptions = navData => {
@@ -21,4 +27,24 @@ ProductDetailScreen.navigationOptions = navData => {
 
 export default ProductDetailScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    image: {
+        width: '100%',
+        height: 300
+    },
+    actions: {
+        marginVertical: 10,
+        alignItems: 'center'
+    },
+    price: {
+        fontSize: 20,
+        color: '#888',
+        textAlign: 'center',
+        marginVertical: 20
+    },
+    description: {
+        fontSize: 14,
+        textAlign: 'center',
+        marginHorizontal: 20
+    }
+})
