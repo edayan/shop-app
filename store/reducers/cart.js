76aa40989db8bf62bc2db4cba316c4206d3ce3cc
edayan <edayan.exgratia@gmail.com>
2020-10-03 23:02:21 +0530

    Fixing bug in reducer

--- store/reducers/cart.js
+++ store/reducers/cart.js
@@ -11,23 +11,25 @@ export default (state = initialState, action) => {
     switch (action.type) {
         case ADD_TO_CART:
             const addedProduct = action.product;
-            const { prodPrice, prodTitle } = addedProduct;
+            const prodPrice = addedProduct.price
+            const prodTitle = addedProduct.title;
             let cartItem;
+
             if (state.items[addedProduct.id]) {
                 //item already added to cart
                 cartItem = new CartItem(
-                    state.items[addedProduct.id].quantity + 1,
-                    prodPrice,
+                    +state.items[addedProduct.id].quantity + 1,
+                    +prodPrice,
                     prodTitle,
-                    state.items[addedProduct.id].sum + prodPrice
+                    +(state.items[addedProduct.id].sum + prodPrice)
                 );
             } else {
-                cartItem = new CartItem(1, prodPrice, prodTitle, prodPrice);
+                cartItem = new CartItem(1, +prodPrice, prodTitle, +prodPrice);
             }
             return {
                 ...state,
                 items: { ...state.items, [addedProduct.id]: cartItem },
-                totalAmount: state.totalAmount + prodPrice
+                totalAmount: +(state.totalAmount + prodPrice)
             }
 
             break;
