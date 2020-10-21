afc0abcaf9dc9f58762ee38aaf17cf7eaf46962e
edayan <edayan.exgratia@gmail.com>
2020-10-21 22:08:31 +0530

    Order item

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -3,13 +3,13 @@ import { FlatList, StyleSheet, Platform } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import { useSelector } from 'react-redux';
 import HeaderButton from '../../components/UI/HeaderButton';
-
+import OrderItem from '../../components/shop/OrderItem'
 
 const OrdersScreen = (props) => {
-    const orders = useSelector(state => state.orders)
+    const orders = useSelector(state => state.orders.orders)
     return (
         <FlatList data={orders} keyExtractor={item => item.id}
-            renderItem={itemData => <Text>{itemData.item.totalAmount}</Text>} />
+            renderItem={itemData => <OrderItem amount={itemData.item.totalAmount} date={itemData.item.readableDate} />} />
     )
 }
 
