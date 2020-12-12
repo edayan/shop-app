4b8b9e09fb10175f709f38996cf51ccf7ffa7cd7
edayan <edayan.exgratia@gmail.com>
2020-12-12 21:00:22 +0530

    fetching orders from server

--- screens/shop/OrdersScreen.js
+++ screens/shop/OrdersScreen.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import { FlatList, StyleSheet, Platform } from 'react-native';
+import React, { useEffect, useState, useCallback } from 'react';
+import { View, FlatList, StyleSheet, Platform, ActivityIndicator } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import HeaderButton from '../../components/UI/HeaderButton';
 import OrderItem from '../../components/shop/OrderItem'
+import * as ordersActions from '../../store/actions/orders';
+import Colors from '../../constants/Colors';
 
 const OrdersScreen = (props) => {
+    const [isLoading, setIsLoading] = useState(false);
+
     const orders = useSelector(state => state.orders.orders);
+
+    const dispatch = useDispatch();
+
+
+
+    useEffect(() => {
+        setIsLoading(true);
+        /**
+         * cannot use async in useEffect, either use then block
+         * or use useCallback, refer ProductOverviewScreen.js
+         */
+        dispatch(ordersActions.fetchOrders()).then(() => {
+            setIsLoading(false)
+        }).catch((err) => {
+            console.error(err)
+        })
+
+
+    }, [dispatch]);
+
+    if (isLoading) {
+        return (
+            <View style={styles.centered}>
+                <ActivityIndicator size="large" color={Colors.primary} />
+            </View>
+        )
+    }
     return (
         <FlatList data={orders} keyExtractor={item => item.id}
             renderItem={itemData => <OrderItem
@@ -31,4 +62,10 @@ OrdersScreen.navigationOptions = navdata => {
 }
 export default OrdersScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    centered: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    }
+})
--- store/actions/orders.js
+++ store/actions/orders.js
@@ -1,4 +1,41 @@
+import Order from '../../models/order';
+
 export const ADD_ORDER = 'ADD_ORDER';
+export const SET_ORDERS = 'SET_ORDERS';
+
+export const fetchOrders = () => {
+    return async dispatch => {
+
+        try {
+            const response = await fetch('https://rn-complete-guide-shop-2346f.firebaseio.com/orders/u1.json?key=AIzaSyADbcDdwNBpr3iVwSny1ejlwC4bBpa5x0c');
+            if (!response.ok) {
+                throw new Error('something went wrong');
+            }
+            const resData = await response.json();
+
+            const loadedOrders = [];
+            for (key in resData) {
+                loadedOrders.push(
+                    new Order(
+                        key, resData[key].cartItems,
+                        resData[key].totalAmount,
+                        new Date(resData[key].date)
+                    )
+                );
+            }
+            dispatch({
+                type: SET_ORDERS,
+                orders: loadedOrders
+            })
+        } catch (err) {
+            console.log(err);
+            throw err
+        }
+
+
+
+    }
+}
 
 
 export const addOrder = (cartItems, totalAmount) => {
